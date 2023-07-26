4ef0edc262d4e87fa95f7f1f5a229ee877efac61
Alina <krakhmaalina@gmail.com>
2023-07-26 10:38:19 +0300

    fixed cart footer component

--- src/components/cart-footer/cart-footer.tsx
+++ src/components/cart-footer/cart-footer.tsx
@@ -1,11 +1,20 @@
 import { CartFooterProps } from '../../ts/interfaces';
-import { priceFormatter } from '../../ts/view';
+import { priceFormatter, declension } from '../../ts/view';
 import './style.scss';
 
 const CartFooter = ({ totalItems, totalPrice }: CartFooterProps) => {
+  const resultDeclension = declension(
+    totalItems,
+    'единица',
+    'единицы',
+    'единиц'
+  );
+
   return (
     <footer className="cart-footer">
-      <div className="cart-footer__count">{totalItems} единицы</div>
+      <div className="cart-footer__count">
+        {totalItems} {resultDeclension}
+      </div>
       <div className="cart-footer__price">
         {priceFormatter.format(totalPrice)} руб.
       </div>
--- src/ts/view.ts
+++ src/ts/view.ts
@@ -1,1 +1,25 @@
 export const priceFormatter = new Intl.NumberFormat();
+
+export const declension = (
+  number: number,
+  one: string,
+  two: string,
+  three: string
+) => {
+  const number1 = number % 10;
+
+  if (number1 === 1 && number !== 11) {
+    return one;
+  } else if (
+    number1 > 1 &&
+    number1 < 5 &&
+    number !== 12 &&
+    number !== 13 &&
+    number !== 14
+  ) {
+    return two;
+  } else if (number > 10 && number < 20) {
+    return three;
+  }
+  return three;
+};
